1fb284ae9f348df70ce25c7287487e0fca18ed7e
Alex Net <alexnet2004@gmail.com>
2022-06-07 23:09:14 +0300

    Мсправили оформление кода (подогнали под psr-12)
    добавили отдельным файлом .. текст задания..

--- Complex.php
+++ Complex.php
@@ -2,170 +2,171 @@
 /**
  * класс для работы с комплексными числами . 
  */
-class Complex {
+class Complex 
+{
 
-	/**
-	 * действительна часть комплексного числа
-	 * @var [type]
-	 */
-	private $re=0;
+    /**
+     * действительна часть комплексного числа
+     * @var [type]
+     */
+    private $re = 0;
 
-	/**
-	 *  мнимая часть комплексного числа.. 
-	 * @var [type]
-	 */
-	private $im=0;
+    /**
+     *  мнимая часть комплексного числа.. 
+     * @var [type]
+     */
+    private $im = 0;
 
-	public function __construct (...$var)
-	{
-		if(!empty($var[0]) && is_string($var[0]) && preg_match('#(-?\d*\.?\d*)([+-]?\d*\.?\d*i)?#i',$var[0],$els)){
-			array_shift($els);
-			// мнимая часть может располздтись на два элемента если  не задана реальная составляющая ..
-			if (count($els)==2 && $els[1]=='i')
-				$els=[0,implode('',$els)];
-			// есть мнимая составляющая .. и там нет цифр = надо заменить i на единицу .. 
-			if (!empty($els[1]) && !preg_match('#\d#',$els[1]))
-				$els[1]=str_replace('i', '1', $els[1]);
+    public function __construct (...$var)
+    {
+        if(!empty($var[0]) && is_string($var[0]) && preg_match('#(-?\d*\.?\d*)([+-]?\d*\.?\d*i)?#i', $var[0], $els)) {
+            array_shift($els);
+            // мнимая часть может располздтись на два элемента если  не задана реальная составляющая ..
+            if (count($els) == 2 && $els[1] == 'i') {
+                $els = [0, implode('', $els)];
+            }
+            // есть мнимая составляющая .. и там нет цифр = надо заменить i на единицу .. 
+            if (!empty($els[1]) && !preg_match('#\d#', $els[1])) {
+                $els[1] = str_replace('i', '1', $els[1]);
+            }
 
-			$els=array_pad($els, 2, 0);
-			$var=array_map('floatval', $els);			
-		}
+            $els = array_pad($els, 2, 0);
+            $var = array_map('floatval', $els);         
+        }
 
-		$var=array_pad($var, 2, 0);
-		
-		$this->re=floatval($var[0]);
-		$this->im=floatval($var[1]);
-		
-
-	}
-	
-	/**
-	 * Печать числа .
-	 * @return string [description]
-	 */
-	public function __toString()
-	{
-		$str='';
-		if ($this->re)
-			$str=sprintf('%g',round($this->re,2));
-		if ($this->im){
-			$fmt='%gi';
-			if ($str)
-				$fmt='%+gi';
-			$str2=sprintf($fmt,round($this->im,2));
-			if (abs($this->im)==1)
-				$str2=str_replace('1','', $str2);
-			$str.=$str2;
-		}
-		$str=$str?$str:'0';
-		return $str;
-	}
-
-
-	/**
-	 * вернуть действительную часть
-	 * @return int Действительная составляющая комплексного числа
-	 */
-	public function getRe():float 
-	{
-		return $this->re;
-	}
-
-	/**
-	 * вернуть мнимую часть .. 
-	 * @return int Мнимая составляющая комплексного числа 
-	 */
-	public function getIm():float
-	{
-		return $this->im;
-	}
+        $var = array_pad($var, 2, 0);
+        
+        $this->re = floatval($var[0]);
+        $this->im = floatval($var[1]);
+    }
+    
+    /**
+     * Печать числа .
+     * @return string [description]
+     */
+    public function __toString()
+    {
+        $str = '';
+        if ($this->re) {
+            $str = sprintf('%g', round($this->re, 2));
+        }
+        if ($this->im) {
+            $fmt = '%gi';
+            if ($str) {
+                $fmt = '%+gi';
+            }
+            $str2 = sprintf($fmt, round($this->im, 2));
+            if (abs($this->im) == 1) { 
+                $str2 = str_replace('1', '', $str2);
+            }
+            $str .= $str2;
+        }
+        $str = $str ? $str : '0';
+        return $str;
+    }
 
 
-	/**
-	 * сложение ...
-	 * @param Complex|string|int $c Второе слогаемое
-	 */
-	public function add($c)
-	{
-		if ($c instanceof Complex){
-			$this->re+=$c->getRe();
-			$this->im+=$c->getIm();
-			return $this;
-		}
-		$tmp=new static($c);
-		if ($tmp)
-			$this->add($tmp);
-		
-		return $this;
-	}
+    /**
+     * вернуть действительную часть
+     * @return int Действительная составляющая комплексного числа
+     */
+    public function getRe(): float 
+    {
+        return $this->re;
+    }
+
+    /**
+     * вернуть мнимую часть .. 
+     * @return int Мнимая составляющая комплексного числа 
+     */
+    public function getIm(): float
+    {
+        return $this->im;
+    }
 
 
-	/**
-	 * вычетание
-	 * @param Complex $c вычитаемое
-	 */
-	public function sub($c)
-	{
-		if ($c instanceof Complex){
-			$this->re-=$c->getRe();
-			$this->im-=$c->getIm();
-			return $this;
-		}
+    /**
+     * сложение ...
+     * @param Complex|string|int $c Второе слогаемое
+     */
+    public function add($c)
+    {
+        if ($c instanceof Complex) {
+            $this->re += $c->getRe();
+            $this->im += $c->getIm();
+            return $this;
+        }
+        $tmp = new static($c);
+        if ($tmp) {
+            $this->add($tmp);
+        }
+        return $this;
+    }
 
-		$tmp=new static($c);
-		$this->sub($tmp);
-		return $this;
-	}
 
-	/**
-	 * умножение 
-	 * @param string|int|Complex  $c Второй множитель
-	 * @return [type] [description]
-	 */
-	public function mul($c)
-	{
-		if ($c instanceof Complex){
-			$re=$c->getRe();
-			$im=$c->getIm();
-			$oldRe=$this->re;
-			$this->re=$this->re*$re-$this->im*$im;
-			$this->im=$oldRe*$im+$this->im*$re;
-			return $this;
-		}
-		$tmp=new static($c);
-		$this->mul($tmp);
+    /**
+     * вычетание
+     * @param Complex $c вычитаемое
+     */
+    public function sub($c): Complex
+    {
+        if ($c instanceof Complex) {
+            $this->re -= $c->getRe();
+            $this->im -= $c->getIm();
+            return $this;
+        }
 
-		return $this;
-	}
+        $tmp = new static($c);
+        $this->sub($tmp);
+        return $this;
+    }
 
-	/**
-	 * деление  
-	 * @param  Complex|string|int $c делитель
-	 * @return Complex
-	 */
-	public function div($c)
-	{
-		if ($c instanceof Complex){
-			$re=$c->getRe();
-			$im=$c->getIm();
-			if($im){
-				$znam=new Complex($re,-$im);
-				$c->mul($znam);
-				$this->mul($znam);
-				$this->div($re**2-$im**2);
-			}
-			else{
-				$this->re/=$re;
-				$this->im/=$re;
-			}
+    /**
+     * умножение 
+     * @param string|int|Complex  $c Второй множитель
+     * @return [type] [description]
+     */
+    public function mul($c)
+    {
+        if ($c instanceof Complex) {
+            $re = $c->getRe();
+            $im = $c->getIm();
+            $oldRe = $this->re;
+            $this->re = $this->re * $re - $this->im * $im;
+            $this->im = $oldRe * $im + $this->im * $re;
+            return $this;
+        }
+        $tmp=new static($c);
+        $this->mul($tmp);
 
-			return $this;
-		}
+        return $this;
+    }
 
-		$tmp=new static($c);		
-		$this->div($tmp);
-		return  $this;
+    /**
+     * деление  
+     * @param  Complex|string|int $c делитель
+     * @return Complex
+     */
+    public function div($c)
+    {
+        if ($c instanceof Complex) {
+            $re = $c->getRe();
+            $im = $c->getIm();
+            if($im) {
+                $znam = new Complex($re, -$im);
+                $c->mul($znam);
+                $this->mul($znam);
+                $this->div($re ** 2 - $im ** 2);
+            } else {
+                $this->re /= $re;
+                $this->im /= $re;
+            }
 
-	}
+            return $this;
+        }
 
+        $tmp = new static($c);      
+        $this->div($tmp);
+        return  $this;
+    }
 }
--- tests/ComplexTest.php
+++ tests/ComplexTest.php
@@ -1,75 +1,77 @@
 <?php 
 include 'Complex.php';
+
 class ComplexTest extends \PHPUnit\Framework\TestCase
 {
 
-	public function creator()
-	{
-		return [
-			'dig i'=>[[0,1],'i'],
-			'dig 5+9i'=>[[5.5,9],'5.5+9i'],
-			'dig 10,i'=>[[10,0.5],'10+0.5i'],
-			'dig 0,5i'=>[[0,5],'5i'],
-			'dig 0,-5i'=>[[0,-5],'-5i'],
-			'dig -6,-5i'=>[[-6,-5],'-6-5i'],
-			'dig -0'=>['-0','0'],
-			'dig i'=>['i','i'],
-			'dig 3.5'=>['3.5','3.5'],
-			'dig1 3.5'=>[3.5,'3.5'],
-			'dig -i'=>['-i','-i'],
-			'dig 5-i'=>['5-i','5-i'],
-			'dig -0-i'=>['-0-i','-i'],
-			'dig -5i'=>['-5i','-5i'],
-			'dig 6'=>['6','6'],
-			'dig 0.6'=>['0.6+i','0.6+i'],
-			'error '=>['qdqweqwe','0'],
-			'dig -8.8i'=>['-8.8i','-8.8i']
-		];
-	}
+    public function creator()
+    {
+        return [
+            'dig i' => [[0, 1], 'i'],
+            'dig 5+9i' => [[5.5, 9], '5.5+9i'],
+            'dig 10,i' => [[10, 0.5], '10+0.5i'],
+            'dig 0,5i' => [[0, 5], '5i'],
+            'dig 0,-5i' => [[0, -5], '-5i'],
+            'dig -6,-5i' => [[-6, -5], '-6-5i'],
+            'dig -0' => ['-0', '0'],
+            'dig i' => ['i', 'i'],
+            'dig 3.5' => ['3.5', '3.5'],
+            'dig1 3.5' => [3.5, '3.5'],
+            'dig -i' => ['-i', '-i'],
+            'dig 5-i' => ['5-i', '5-i'],
+            'dig -0-i' => ['-0-i', '-i'],
+            'dig -5i' => ['-5i', '-5i'],
+            'dig 6' => ['6', '6'],
+            'dig 0.6' => ['0.6+i', '0.6+i'],
+            'error ' => ['qdqweqwe', '0'],
+            'dig -8.8i' => ['-8.8i', '-8.8i']
+        ];
+    }
 
-	/**
-	 * @dataProvider creator
-	 */
-	public function testCreateObjs($v,$r)
-	{
-		$a=6;
-		if (is_array($v))
-			$a=new Complex($v[0],$v[1]);
-		else 
-			$a=new Complex($v);
-		$this->assertEquals($a,$r,'ошибка '.$a );
-		return rand();
-	}
+    /**
+     * @dataProvider creator
+     */
+    public function testCreateObjs($v, $r)
+    {
+        $a = 6;
+        if (is_array($v)) {
+            $a = new Complex($v[0], $v[1]);
+        } else {
+            $a = new Complex($v);
+        }
+        $this->assertEquals($a, $r, 'ошибка ' . $a);
+        return rand();
+    }
 
 
-	public function opersProvider()
-	{
-		return [// 5-6i
-			['add','0','5-6i'],
-			['add','i','5-5i'],
-			['add','-5i','5-10i'],
-			['sub','3.5','1.5-10i'],
-			['mul',2,'3-20i'],
-			['mul','1-2i','-37-26i'],
-			['div',new Complex(4,0),'-9.25-6.5i'],
-			['div',new Complex(-4,3),'2.5+7.68i'],
-		];
-	}
+    public function opersProvider()
+    {
+        return [// 5-6i
+            ['add', '0', '5-6i'],
+            ['add', 'i', '5-5i'],
+            ['add', '-5i', '5-10i'],
+            ['sub', '3.5', '1.5-10i'],
+            ['mul', 2, '3-20i'],
+            ['mul', '1-2i', '-37-26i'],
+            ['div', new Complex(4, 0), '-9.25-6.5i'],
+            ['div', new Complex(-4, 3), '2.5+7.68i'],
+        ];
+    }
 
-	public function testOperInit()
-	{
-		$c=new Complex('5-6i');
-		$this->assertEquals($c,'5-6i');
-		return $c;
-	}
+    public function testOperInit()
+    {
+        $c = new Complex('5-6i');
+        $this->assertEquals($c, '5-6i');
+        return $c;
+    }
 
-	/**
-	 * @dataProvider opersProvider
-	 * @depends testOperInit
-	 */
-	public function testQueueOpers($oper,$dig,$res,$c)
-	{
-		call_user_func([$c,$oper], $dig);
-		$this->assertEquals($c,$res,'ошибка'.$c);
-	}
+    /**
+     * @dataProvider opersProvider
+     * @depends testOperInit
+     */
+    public function testQueueOpers($oper, $dig, $res, $c)
+    {
+        call_user_func([$c, $oper], $dig);
+        $this->assertEquals($c, $res, 'ошибка' . $c);
+    }
 }
